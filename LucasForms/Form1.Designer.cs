bcdecc20abe4f97430a6b64106db0057385597b8
luk-py <lukas.conrad@hotmail.com>
2022-12-10 12:49:08 -0300

    SmatFit clientes V1

---
+++ LucasForms/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace LucasForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.idadeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sexoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clientsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Azure;
+            this.button1.Location = new System.Drawing.Point(409, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Adicionar Cliente";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(47, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Idade";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(51, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome";
+            this.label2.UseCompatibleTextRendering = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.checkBox1.Location = new System.Drawing.Point(294, 102);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 23);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Masculino";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.MouseHover += new System.EventHandler(this.checkBox1_MouseHover);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.checkBox2.Location = new System.Drawing.Point(294, 135);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(100, 23);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Feminino";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(103, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 27);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(103, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 26);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Location = new System.Drawing.Point(9, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Filtrar por Genero";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(148, 195);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(379, 22);
+            this.textBox3.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(533, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 36);
+            this.button2.TabIndex = 13;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.CadetBlue;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idadeDataGridViewTextBoxColumn,
+            this.nomeDataGridViewTextBoxColumn,
+            this.sexoDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.clientsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(567, 274);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Consolas", 30F);
+            this.label4.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label4.Location = new System.Drawing.Point(198, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 47);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Clientes";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Azure;
+            this.label5.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label5.Location = new System.Drawing.Point(395, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 39);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Obs: Caso não se identifique \r\ncom nenhuma opção de genero\r\n não marque a caixinh" +
+    "a! :)";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // idadeDataGridViewTextBoxColumn
+            // 
+            this.idadeDataGridViewTextBoxColumn.DataPropertyName = "idade";
+            this.idadeDataGridViewTextBoxColumn.HeaderText = "idade";
+            this.idadeDataGridViewTextBoxColumn.Name = "idadeDataGridViewTextBoxColumn";
+            this.idadeDataGridViewTextBoxColumn.Width = 124;
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            this.nomeDataGridViewTextBoxColumn.DataPropertyName = "nome";
+            this.nomeDataGridViewTextBoxColumn.HeaderText = "nome";
+            this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            this.nomeDataGridViewTextBoxColumn.Width = 250;
+            // 
+            // sexoDataGridViewTextBoxColumn
+            // 
+            this.sexoDataGridViewTextBoxColumn.DataPropertyName = "sexo";
+            this.sexoDataGridViewTextBoxColumn.HeaderText = "sexo";
+            this.sexoDataGridViewTextBoxColumn.Name = "sexoDataGridViewTextBoxColumn";
+            this.sexoDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // clientsBindingSource
+            // 
+            this.clientsBindingSource.DataSource = typeof(LucasForms.Clients);
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(LucasForms.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(591, 514);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SmartFit Clientes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource clientsBindingSource;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idadeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sexoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ LucasForms/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+/*
+    PROJETO SIMULANDO UM CADASTRO DE CLIENTES 
+    QUE PERMITE CADASTRAR :NOME :IDADE :GENERO
+    RETORNANDO UM LISTVIEW COM OS CLIENTES CADASTRADOS EM ORDEM CRESCENTE DE IDADE
+    E UM FILTER PRA FILTRAR POR GENERO
+ */
+namespace LucasForms
+{
+    public partial class Form1 : Form
+    {
+        DataTable cls = new DataTable();//INICIALIZANDO UMA BASE DE DADOS
+        public Form1()
+        {
+            InitializeComponent();
+            AddTable();//METODO QUE CRIA E ADICIONA OS DADOS DOS NA TABELA CLS
+            
+        }
+
+        private void AddTable()
+        {
+            /* 
+                ESTE METODO CRIA AS COLUNAS NO DATAGRID 
+             */
+            cls.Columns.Add("idade", typeof(int));
+            cls.Columns.Add("nome", typeof(string));
+            cls.Columns.Add("sexo", typeof(string));
+
+            /* 
+                LAÇO QUE PERCOERRE A LISTA DO OBJETO CLIENTS 
+                E PARA CADA OBJETO PREENCHE OS CAMPOS IDADE NOME E SEXO
+            */
+            var listClients = Clients.GetListaClients();
+            foreach (Clients clientes in listClients)
+            {
+                cls.Rows.Add(clientes.idade, clientes.nome, clientes.sexo);
+            }
+
+            
+            dataGridView1.DataSource = cls;
+            dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);//RETORNA A LISTA EM ORDEM POR IDADE
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)   //ENQUANTO UM CHECK BOX1 ESTÁ ATIVO, DESATIVA O OUTRO
+        {
+            if (((CheckBox)sender).Checked)
+            {
+                checkBox1.Enabled = false;
+            }
+            else
+            {
+                checkBox1.Enabled = true;
+            } 
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //EVENTOS DISPARADOS AO CLICAR EM ADICIONAR
+        {
+            // ESTE LAÇO PERMITE QUE A VARIAVEL CHECK RECEBA O VALOR NO QUAL O CHECKBOX ESTA MARCADO
+            string check;
+            
+            if (checkBox1.Checked)
+            {
+                check = checkBox1.Text.ToString();
+            }
+            else if (checkBox2.Checked)
+            {
+                check = checkBox2.Text.ToString();
+            }
+            else
+            {
+                check = "Genero não selecionado";
+            };
+
+            //O DATAGRID VAI RECEBER OS DADOS QUE ESTÃO LISTADOS A BAIXO AO CLICAR EM ADICIONAR
+            string[] client =
+            {
+
+                textBox2.Text,
+                textBox1.Text,
+                check
+            };
+            
+
+            cls.Rows.Add(int.Parse(textBox2.Text), textBox1.Text, check);
+            dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);//RETORNA A LISTA EM ORDEM POR IDADE
+
+            //LIMPA TODOS OS CAMPOS 
+            textBox1.Text = "";
+            textBox2.Text = "";
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+        }
+
+        private bool IsNumber(string valor)
+        {
+            return valor.All(char.IsNumber);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) //ENQUANTO UM CHECK BOX 2 ESTÁ ATIVO, DESATIVA O OUTRO
+        {
+            if (((CheckBox)sender).Checked)
+            {
+                checkBox2.Enabled = false;
+            }
+            else
+            {
+                checkBox2.Enabled = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FilterGrid();
+        }
+
+        private void FilterGrid() //METODO RESPONSAVEL POR FILTRAR OS DADO DE ACORDO COM O GENERO
+        {
+            cls.DefaultView.RowFilter = string.Format("[{0}] LIKE '%{1}%'", "sexo", textBox3.Text);
+            dataGridView1.DataSource = cls;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+    public class Clients //CLASSE CLIENTES QUE RECEBE O OBJETO CLIENTS
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public string sexo { get; set; }
+
+        public Clients(string nome, int idade, string sexo) 
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.sexo = sexo;
+        } 
+        public static List<Clients> GetListaClients() //METODO LISTA QUE RECEBE DADOS E INSERE NA TABELA
+        {
+            var listaClients = new List<Clients>();
+
+            
+            //INSERI ALGUNS DADOS PREDEFINIDOS PARA NÃO PRECISAR ADICIONAR TODOS MANUALMENTE 
+
+            listaClients.Add(new Clients("Neymar Junior", 30, "Masculino"));
+            listaClients.Add(new Clients("Frenkie de Jong", 25, "Masculino"));
+            listaClients.Add(new Clients("Taylor Swift", 32, "Feminino"));
+            listaClients.Add(new Clients("Megan Fox", 36, "Feminino"));
+            listaClients.Add(new Clients("Jenna Ortega", 19, "Feminino"));
+
+
+            return listaClients ; 
+        }
+
+
+
+    }
+
+}
